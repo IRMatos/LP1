646df18e06e8f0d58b5b56386bcfecbf62a921d3
IRMatos <48076749+IRMatos@users.noreply.github.com>
2019-03-29 21:45:21 -0300

    Create questao2.c

---
+++ teste1/questao2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void){
+    int n1;
+    int n2;
+    int i;
+    int rec;
+    printf("Escreva o primeiro:");
+    scanf("%d",n1)
+    printf("Escreva o segundo:");
+    scanf("%d",n2);
+    for(i=n1;i=n2;i++){
+            if i%2==0{
+            printf("Par %d",/i);       
+            }
+      
+    }  
+
+}
